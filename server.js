035510b5dd86d9e742b0551be6215000cb3644f6
Avula Sai Raghu Kiran <avulasairaghukiran1998@gmail.com>
2023-06-20 03:56:11 +0530

    3rd COMMIT

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose
   })
   .catch((err) => console.log(err));
 app.get("/", (req, res) => {
-  res.send("Welcome to server");
+  res.send("Welcome to Stackoverflow server");
 });
 app.get("/homepage", (req, res) => {
   res.send("Welcome to Home");
